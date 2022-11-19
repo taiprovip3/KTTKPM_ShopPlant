8efdbd5fdfc3a8b4ac9306cdd86f44340edf2868
Phan Tấn Tài <taito1doraemon@gmail.com>
2022-11-19 21:39:45 +0700

    fix soimething

--- service_jwt_token/src/main/java/com/se/Application.java
+++ service_jwt_token/src/main/java/com/se/Application.java
@@ -2,10 +2,12 @@ package com.se;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.jms.annotation.EnableJms;
 
 @SpringBootApplication
 @EnableJms
+@EnableEurekaClient
 public class Application {
 
 	public static void main(String[] args) {
