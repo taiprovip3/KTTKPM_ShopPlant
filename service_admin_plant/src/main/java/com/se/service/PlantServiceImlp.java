fef2801b8dd1929f17b2cff67495667f17a4302a
phongok <phongdevgithub666666@gmail.com>
2022-11-21 21:31:35 +0700

    update cache

--- service_admin_plant/src/main/java/com/se/service/PlantServiceImlp.java
+++ service_admin_plant/src/main/java/com/se/service/PlantServiceImlp.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 import com.se.entity.Plant;
@@ -12,13 +13,15 @@ import com.se.repository.PlantRepository;
 @Service
 public class PlantServiceImlp implements PlantService {
 	@Autowired
-	private PlantRepository plantRepository;	 
+	private PlantRepository plantRepository;
+	
 	@Override
+	@Cacheable(value = "plantCache")
 	public List<Plant> findAllPlant() {
 		// TODO Auto-generated method stub
 		return plantRepository.findAll();
 	}
-
+	
 	@Override
 	public Plant findPlantById(int idPlant) {
 		// TODO Auto-generated method stub
